138024180eb23ecbead85d5cfad1679b55033f06
Fossil <colewestbrookphoto@gmail.com>
2021-07-20 19:43:32 -0400

    Beginning food project

---
+++ food-order-app/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import mealsImg from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
+
+const Header = props => {
+  return <React.Fragment>
+      <header className={classes.header}>
+          <h1>FossilFood</h1>
+          <HeaderCartButton>Cart</HeaderCartButton>
+      </header>
+      <div className={classes['main-image']}>
+          <img src={mealsImg} alt="A table full of food!" />
+      </div>
+  </React.Fragment>
+};
+
+export default Header;
