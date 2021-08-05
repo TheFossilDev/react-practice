8d936b881240ea12b16bf000f06eb4fa4aaac81e
Cole Westbrook <colewestbrook@Coles-MacBook-Air.local>
2021-08-05 13:36:05 -0400

    HTTP lesson set

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
-import Meals from './components/Meals/Meals';
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const showCartHandler = () => {
+    setShowCart(true);
+  };
+  const hideCartHandler = () => {
+    setShowCart(false);
+  };
+
   return (
-    <React.Fragment>
-      <Header />
+    <CartProvider>
+      {showCart && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsBump, setBtnIsBump] = useState(false);
+
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  // currNumber = increments through reduce
+  const numCartItems = items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsBump(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsBump(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+
+  }, [items]);
+
   return (
-    <button className={classes.button}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numCartItems}</span>
     </button>
   );
 };
